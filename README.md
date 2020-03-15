ac4bb836f11284a31a4c2578fa60c83ea9084ae6
Edward Chen <edwardmartyr@outlook.com>
2020-03-15 22:11:19 +0800

    修復PDF合併問題。

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # get-people-daily
 获取人民日报电子版
 
-# 使用方法：python3 get-people-daily.py -d yyyy.mm.dd
+# 使用方法
 
-1. yyyy:年份，四位；
-2. mm：月份，一位或两位；
+```sh
+python3 get-people-daily.py -d yyyy.mm.dd
+```
+
+1. yyyy: 年份，四位；
+2. mm: 月份，一位或两位；
 3. dd: 天，一位或两位；
 
-最后会在当前目录得到一个yyyymmdd的文件夹，文件夹中放置了各个版面的报纸pdf版面；
-
-pdf命名方式示例：rmrb2017051801.pdf
+这个 Fork 修复了本来无法使用 `PyPDF2` 库合并 PDF 页面的问题，并可以显示文件大小。最后可以输出完整的 `yyyy-mm-dd.pdf`，而不是未合并的单独 PDF 页面。
 
--- get-people-daily.py
+++ get-people-daily.py
@@ -1,74 +1,89 @@
-#-*- coding:utf-8 -*-
-
 import requests
 import os
 import sys
 import getopt
 from PyPDF2 import PdfFileMerger
+import warnings
+
+merger = PdfFileMerger(strict=False)
+
 
 class Paper(object):
-    
+
     def __init__(self, year, month, day):
         self.year = '%04d' % year
         self.month = '%02d' % month
         self.day = '%02d' % day
+        self.d = self.year+self.month+self.day
         self.base_url = 'http://paper.people.com.cn/rmrb/page/'
-        self.paper_name = '人民日报-'+self.year+'年'+self.month+'月'+self.day+'日.pdf'
+        self.size = 0
 
-    def get_pdf(self,url,filename):
+    def get_pdf(self, url, filename):
         r = requests.get(url)
         if not r.ok:
             return r.ok
         with open(filename, "wb") as f:
             f.write(r.content)
-        print('Save Page: '+filename)
+        pdf_size = len(r.content)/1000000
+        self.size += pdf_size
+        print('    Retrieving page: '+filename +
+              '  |  size: ' + str(round(pdf_size, 2)) + 'mb')
         return r.ok
 
-    def get_page(self,pagenum):
+    def get_page(self, pagenum):
         pagenum = '%02d' % pagenum
-        d = self.year+self.month+self.day
-        p =d+'/rmrb'+d+pagenum+'.pdf'
-        if not os.path.exists(d):
-            os.mkdir(d)
+        p = self.d+'/rmrb'+self.d+pagenum+'.pdf'
+        if not os.path.exists(self.d):
+            os.mkdir(self.d)
         pageurl = ''.join([
-                self.base_url,self.year,'-',
-                self.month,'/',self.day,'/',
-                pagenum,'/rmrb',self.year,
-                self.month,self.day,pagenum,'.pdf'])
-        return self.get_pdf(pageurl,p),p
-
-    def merge_pages(self,pdfs,o):
-        m = PdfFileMerger(strict=False)
-        for pdf in pdfs:
-            m.append(pdf)
-        m.write(o)
-        m.close()
-                
+            self.base_url, self.year, '-',
+            self.month, '/', self.day, '/',
+            pagenum, '/rmrb', self.d, pagenum, '.pdf'])
+        return self.get_pdf(pageurl, p), p
 
     def get_paper(self):
+        print('Downloading pages into temporary directory ...')
         i = 1
         l = []
         while True:
-            ok,p = self.get_page(i)
+            ok, p = self.get_page(i)
             if not ok:
                 break
             l.append(p)
+            merger.append(p)
             i += 1
-       # self.merge_pages(l,d+'/'self.paper_name)
+
+        print('Combining pages ...')
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            merger.write('-'.join([self.year, self.month, self.day])+'.pdf')
+            merger.close()
+
+        print('Deleting temporary directory ...')
+
+        for pdf in l:
+            os.remove(pdf)
+
+        os.rmdir(self.d)
+
+        print('Done. Total size ' + str(round(self.size, 2)) + 'mb.')
+
 
 def main():
-    opts, args = getopt.getopt(sys.argv[1:],'d:',['date='])
+    opts, args = getopt.getopt(sys.argv[1:], 'd:', ['date='])
     for o, a in opts:
-        if o in ['-d','--date']:
+        if o in ['-d', '--date']:
             x = a.split('.')
             year = int(x[0])
             month = int(x[1])
             day = int(x[2])
-    p = Paper(year,month,day)
+    p = Paper(year, month, day)
     p.get_paper()
+
 
 def test():
     pass
+
 
 if __name__ == '__main__':
     main()
